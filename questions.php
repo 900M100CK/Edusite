287af67bc091897432d1afcf0d5c2ee94f47da1c
900M100CK <chinhkhang105e@gmail.com>
2025-07-30 15:48:17 +0700

    Thông điệp mô tả về thay đổi

--- questions.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pageTitle = "Questions";
-ob_start();
-include 'templates/questions.html.php';
-include 'templates/tag_modal.php';
-$contentView = ob_get_clean();
-include 'templates/layout.html.php';
-?>
